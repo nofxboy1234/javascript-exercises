2f662a36a1c329f0ddad44f7f0b6724232a70a2c
Dylan Palmboom <nofxboy1234@gmail.com>
2022-03-15 22:25:45 +0200

    Check if all tests pass - they do
    Keeping solution commented out underneath to compare.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,19 @@
-// const removeFromArray = function (array, remove) {
-//   // let args = Array.from(arguments);
-//   // let args = [...arguments];
-//   // console.log(args);
+const removeFromArray = function (array, remove) {
+  //   // let args = Array.from(arguments);
+  //   // let args = [...arguments];
+  //   // console.log(args);
 
-//   for (let i = 1; i < arguments.length; i++) {
-//     // console.log(arguments[i]);
+  for (let i = 1; i < arguments.length; i++) {
+    // console.log(arguments[i]);
 
-//     const removeIndex = array.indexOf(arguments[i]);
-//     if (removeIndex > -1) {
-//       array.splice(removeIndex, 1);
-//     }
-//   }
+    const removeIndex = array.indexOf(arguments[i]);
+    if (removeIndex > -1) {
+      array.splice(removeIndex, 1);
+    }
+  }
 
-//   return array;
-// };
+  return array;
+};
 
 // // Do not edit below this line
 // module.exports = removeFromArray;
@@ -21,34 +21,34 @@
 // we have 2 solutions here, an easier one and a more advanced one.
 // The easiest way to get an array of all of the arguments that are passed to a function
 // is using the rest operator. If this is unfamiliar to you look it up!
-const removeFromArray = function (...args) {
-  console.log(`args: ${args}`);
-  // the very first item in our list of arguments is the array, we pull it out with args[0]
-  const array = args[0];
-  // create a new empty array
-  const newArray = [];
-  // use forEach to go through the array
-  array.forEach((item) => {
-    console.log(`item: ${item}`);
-    // push every element into the new array
-    // UNLESS it is included in the function arguments
-    // so we create a new array with every item, except those that should be removed
-    if (!args.includes(item)) {
-      newArray.push(item);
-    }
-  });
-  // and return that array
-  console.log(`newArray: ${newArray}`);
-  return newArray;
-};
+// const removeFromArray = function (...args) {
+//   console.log(`args: ${args}`);
+//   // the very first item in our list of arguments is the array, we pull it out with args[0]
+//   const array = args[0];
+//   console.log(`array: ${array}`);
+//   // create a new empty array
+//   const newArray = [];
+//   // use forEach to go through the array
+//   array.forEach((item) => {
+//     // push every element into the new array
+//     // UNLESS it is included in the function arguments
+//     // so we create a new array with every item, except those that should be removed
+//     if (!args.includes(item)) {
+//       console.log(`item: ${item}`);
+//       newArray.push(item);
+//     }
+//   });
+//   // and return that array
+//   console.log(`newArray: ${newArray}`);
+//   return newArray;
+// };
 
 // A simpler, but more advanced way to do it is to use the 'filter' function,
 // which basically does what we did with the forEach above.
 
-// var removeFromArray = function(...args) {
-//   const array = args[0]
-//   return array.filter(val => !args.includes(val))
-// }
-//
+// var removeFromArray = function (...args) {
+//   const array = args[0];
+//   return array.filter((val) => !args.includes(val));
+// };
 
 module.exports = removeFromArray;
