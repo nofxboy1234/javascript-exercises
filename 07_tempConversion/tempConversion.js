1266823254a44c87e76e966a50e07d8d3015ec1f
Dylan Palmboom <nofxboy1234@gmail.com>
2022-03-19 12:29:11 +0200

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  celsiusTemperature = (temperature - 32) * (5 / 9);
+  let roundedTemp = Math.round(celsiusTemperature * 10) / 10;
+  return roundedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  fahrenheitTemperature = temperature * (9 / 5) + 32;
+  let roundedTemp = Math.round(fahrenheitTemperature * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
