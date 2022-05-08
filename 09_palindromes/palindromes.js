a3eca0be52de446b853954db100771beec703e7e
Dylan Palmboom <nofxboy1234@gmail.com>
2022-05-08 19:25:54 +0200

    Complete 09_plaindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str = "") {
+  // let re = /[\W_]/g;
+  let re = /[^A-Za-z0-9]/g;
+  let lowerRegexStr = str.toLowerCase().replace(re, "");
+  let reversedStr = lowerRegexStr.split("").reverse().join("");
+  return reversedStr === lowerRegexStr;
 };
 
 // Do not edit below this line
