5f7b1fca868963c74c3bdd318f616bbe8e005092
Dylan Palmboom <nofxboy1234@gmail.com>
2022-05-08 21:26:12 +0200

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num = 1) {
+  if (num < 0) return "OOPS";
 
+  num = Number(num);
+
+  let fibonacciNumbers = [1];
+  for (let i = 0; i < num - 1; i++) {
+    if (fibonacciNumbers.length === 1) {
+      fibonacciNumbers.push(1);
+    } else {
+      let oneNumBefore = fibonacciNumbers.length - 1;
+      let twoNumBefore = fibonacciNumbers.length - 2;
+      const sum =
+        fibonacciNumbers[oneNumBefore] + fibonacciNumbers[twoNumBefore];
+      fibonacciNumbers.push(sum);
+    }
+  }
+  return fibonacciNumbers[fibonacciNumbers.length - 1];
 };
 
 // Do not edit below this line
