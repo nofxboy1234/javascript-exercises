a8411c47c4921bb6e86ae61eef6aa8753967117a
Dylan Palmboom <nofxboy1234@gmail.com>
2022-05-09 21:54:14 +0200

    Pass first test of findTheOldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array = []) {
+  return array.reduce((oldest, person) => {
+    // return person;
+    return person.yearOfDeath - person.yearOfBirth >
+      oldest.yearOfDeath - oldest.yearOfBirth
+      ? person
+      : oldest;
+  });
 };
 
 // Do not edit below this line
