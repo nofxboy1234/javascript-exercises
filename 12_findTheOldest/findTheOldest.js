968dc8b4223287f3620ca4bca51e91f261788fe7
Dylan Palmboom <nofxboy1234@gmail.com>
2022-05-10 16:40:29 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
 const findTheOldest = function (array = []) {
   return array.reduce((oldest, person) => {
     // return person;
+    const todayDate = new Date();
+    const currentYear = todayDate.getFullYear();
+    oldest.yearOfDeath = !oldest.yearOfDeath ? currentYear : oldest.yearOfDeath;
+
     return person.yearOfDeath - person.yearOfBirth >
       oldest.yearOfDeath - oldest.yearOfBirth
       ? person
